5e41ce50180586339abab6b860eb69fe30bb8cac
Rails47 <158601407+Rails47@users.noreply.github.com>
2024-04-14 12:25:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Text.Json;
+using System.Web.Mvc;
+
+namespace ConsoleApp21
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Type { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+        public string Response { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string title, string year, string type)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var apiKey = "YOUR_OMDB_API_KEY";
+            var url = $"http://www.omdbapi.com/?apikey={apiKey}&t={title}&y={year}&type={type}";
+
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var movie = JsonSerializer.Deserialize<Movie>(content);
+
+                return View(movie);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
